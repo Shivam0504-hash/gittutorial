952fdcbf7c7fa4062739c470e8894717b16dd012
user <user@users-MacBook-Pro.local>
2025-01-23 12:01:04 +0530

    add bottom

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from './src/Header'
 import Bottom from './src/Bottom'
-import Center from './src/Center'
+
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
       <Header/>
-      <Center/>
       <Bottom/>
       </SafeAreaView>
     
